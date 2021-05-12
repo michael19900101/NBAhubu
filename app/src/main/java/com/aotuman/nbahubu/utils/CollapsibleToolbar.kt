aed5a50439d8415b52151650a71b685a44eb9641
HuangKunTing <382968311@qq.com>
2021-05-12 23:06:56 +0800

    修改图标

---
+++ app/src/main/java/com/aotuman/nbahubu/utils/CollapsibleToolbar.kt
@@ -0,0 +1,27 @@
+package com.aotuman.nbahubu.utils
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.motion.widget.MotionLayout
+import com.google.android.material.appbar.AppBarLayout
+
+/**
+ * <pre>
+ *     author: aotuman
+ *     date  : 2021/05/11
+ *     desc  :
+ * </pre>
+ */
+class CollapsibleToolbar @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : MotionLayout(context, attrs, defStyleAttr), AppBarLayout.OnOffsetChangedListener {
+
+    override fun onOffsetChanged(appBarLayout: AppBarLayout?, verticalOffset: Int) {
+        progress = -verticalOffset / appBarLayout?.totalScrollRange?.toFloat()!!
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        (parent as? AppBarLayout)?.addOnOffsetChangedListener(this)
+    }
+}
