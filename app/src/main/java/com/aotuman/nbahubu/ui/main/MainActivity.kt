17d179f423a9b04c17177a21a6385e3c712d677f
huangkunting <382968311@qq.com>
2021-05-20 16:49:15 +0800

    球员列表的实现

--- app/src/main/java/com/aotuman/nbahubu/ui/main/MainActivity.kt
+++ app/src/main/java/com/aotuman/nbahubu/ui/main/MainActivity.kt
@@ -1,19 +1,13 @@
 package com.aotuman.nbahubu.ui.main
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import com.aotuman.nbahubu.R
-import com.hjq.permissions.OnPermissionCallback
-import com.hjq.permissions.Permission
-import com.hjq.permissions.XXPermissions
 import com.aotuman.nbahubu.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
-import org.jetbrains.anko.toast
 
 
 @FlowPreview
@@ -23,41 +17,24 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var mainBinding: ActivityMainBinding
     private val mViewModel: MainViewModel by viewModels()
+    private val playerAdapter by lazy { PlayerAdapter(arrayListOf()) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
 
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
 
-        XXPermissions.with(this)
-            .permission(Permission.READ_EXTERNAL_STORAGE)
-            .permission(Permission.WRITE_EXTERNAL_STORAGE)
-            .request(object : OnPermissionCallback {
-                override fun onGranted(permissions: List<String>, all: Boolean) {
-                    if (all) {
-                        toast("获取读写SD卡权限成功")
-                    } else {
-                        toast("获取部分权限成功，但部分权限未正常授予")
-                    }
-                }
-
-                override fun onDenied(permissions: List<String>, never: Boolean) {
-                    if (never) {
-                        toast("被永久拒绝授权，请手动授予读写SD卡权限")
-                        // 如果是被永久拒绝就跳转到应用权限系统设置页面
-                        XXPermissions.startPermissionActivity(this@MainActivity, permissions)
-                    } else {
-                        toast("获取读写SD卡权限失败")
-                    }
-                }
-            })
-
+        mainBinding.apply {
+            recyleView.adapter = playerAdapter
+        }
 
         mViewModel.testRequest().observe(this, Observer {
-            it
-            Log.e("MainActivity","aaaa")
+            it.data?.let {
+                    players -> playerAdapter.addData(players)
+                    playerAdapter.notifyDataSetChanged()
+            }
+            mainBinding.swiperRefresh.isEnabled = false
         })
     }
 }
---
+++ app/src/main/java/com/aotuman/nbahubu/ui/main/PlayerAdapter.kt
@@ -0,0 +1,77 @@
+package com.aotuman.nbahubu.ui.main
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.aotuman.nbahubu.R
+import com.aotuman.nbahubu.data.entity.Player
+
+
+class PlayerAdapter(private val players: ArrayList<Player>) : RecyclerView.Adapter<PlayerAdapter.PlayerViewHolder>() {
+
+    class PlayerViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        val mCnName: TextView
+
+        val mEnName: TextView
+
+        val mImgPlayer: ImageView
+
+        val mImgTeamLogo: ImageView
+
+        val mPlayPosition: TextView
+
+        val mTeamName: TextView
+
+        init {
+            mCnName = view.findViewById(R.id.tv_cn_name)
+
+            mEnName = view.findViewById(R.id.tv_en_name)
+
+            mImgPlayer = view.findViewById(R.id.iv_player)
+
+            mImgTeamLogo = view.findViewById(R.id.iv_team_logo)
+
+            mPlayPosition = view.findViewById(R.id.tv_play_position)
+
+            mTeamName = view.findViewById(R.id.tv_team_name)
+        }
+
+        fun bind(player: Player) {
+            mCnName.text = player.cnName
+            mEnName.text = player.enName
+            mPlayPosition.text = player.position
+            mTeamName.text = player.teamName
+            mImgPlayer.load(player.icon){
+                placeholder(R.mipmap.default_player)
+            }
+            mImgTeamLogo.load(player.teamLogo){
+                placeholder(R.mipmap.default_teamlogo_90)
+                error(R.mipmap.default_teamlogo_90)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlayerViewHolder {
+        return PlayerViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_player_row, parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: PlayerViewHolder, position: Int) {
+        holder.bind(players[position])
+    }
+
+    override fun getItemCount(): Int = players.size
+
+    fun addData(list: List<Player>) {
+        players.addAll(list)
+    }
+}
